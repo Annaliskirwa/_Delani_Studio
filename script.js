21acab367034dadf321492b98ed88bb2345aafca
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-12 00:14:34 +0300

    It will toggle between development and content

--- script.js
+++ script.js
@@ -4,8 +4,16 @@ $(document).ready(function(){
         $("div#design-content").toggle();
         $("div#design").toggle();
       });
-      $("div#design-content").click(function() {
+    $("div#design-content").click(function() {
         $("div#design-content").toggle();
         $("div#design").toggle();
+      });
+    $("div#development").click(function() {
+        $("div#development-content").toggle();
+        $("div#development").toggle();
+      });
+    $("div#development-content").click(function() {
+        $("div#development-content").toggle();
+        $("div#development").toggle();
       });
 });
